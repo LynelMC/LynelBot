a1cebbaa3010ca1022b5402d0256a69665475d38
LynelMC <narumarodayo@gmail.com>
2026-07-31 18:45:04 +0900

    Update security.py

--- cogs/security.py
+++ cogs/security.py
@@ -3,62 +3,52 @@ from discord import app_commands
 from discord.ext import commands
 from storage import storage
 from utils.checks import is_admin
-from utils.embeds import EmbedUtils
-from utils.logger import Logger
-import re
-import datetime
+from services.security_service import SecurityService
 
 class SecurityCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.url_pattern = re.compile(r'https?://\S+')
-        # フィッシングURLの簡易リスト (本来は外部APIやDBを使用)
-        self.phishing_domains = ["discord-gift.com", "discord-nitro.com", "dlscord.com"]
+        self.service = SecurityService(bot)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if not message.guild or message.author.bot:
             return
+        await self.service.check_message(message)
 
-        guild_id = message.guild.id
-        
-        # 招待リンク対策
-        if storage.get_setting("security", guild_id, "anti_invite", False):
-            if "discord.gg/" in message.content:
-                if not message.author.guild_permissions.manage_messages:
-                    await message.delete()
-                    await message.channel.send(f"{message.author.mention} 招待リンクの送信は禁止されています。", delete_after=5)
-                    return
-
-        # フィッシングURL検知
-        if storage.get_setting("security", guild_id, "anti_phishing", False):
-            urls = self.url_pattern.findall(message.content)
-            for url in urls:
-                for domain in self.phishing_domains:
-                    if domain in url:
-                        await message.delete()
-                        await message.guild.ban(message.author, reason="フィッシングURLの送信")
-                        await Logger.log_action(self.bot, guild_id, "フィッシング検知", f"{message.author.mention} がフィッシングURLを送信したためBANしました。", color=discord.Color.red())
-                        return
-
-        # NGワード対策
-        ng_words = storage.get_setting("security", guild_id, "ng_words", [])
-        for word in ng_words:
-            if word in message.content:
-                await message.delete()
-                await message.channel.send(f"{message.author.mention} 不適切な言葉が含まれています。", delete_after=5)
-                return
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        await self.service.process_join(member)
 
     @app_commands.command(name="security-setup", description="セキュリティ機能の設定")
     @is_admin()
-    async def security_setup(self, interaction: discord.Interaction, anti_invite: bool = None, anti_phishing: bool = None):
+    async def security_setup(self, interaction: discord.Interaction, 
+                               anti_invite: bool = None, 
+                               anti_phishing: bool = None,
+                               anti_raid: bool = None,
+                               min_age_days: int = None):
+        await interaction.response.defer(ephemeral=True)
         guild_id = interaction.guild_id
-        if anti_invite is not None:
-            storage.set_data("security", guild_id, "anti_invite", anti_invite)
-        if anti_phishing is not None:
-            storage.set_data("security", guild_id, "anti_phishing", anti_phishing)
         
-        await interaction.response.send_message("セキュリティ設定を更新しました。", ephemeral=True)
+        if anti_invite is not None: storage.set_data("security", guild_id, "anti_invite", anti_invite)
+        if anti_phishing is not None: storage.set_data("security", guild_id, "anti_phishing", anti_phishing)
+        if anti_raid is not None: storage.set_data("security", guild_id, "anti_raid", anti_raid)
+        if min_age_days is not None: storage.set_data("security", guild_id, "min_account_age", min_age_days)
+        
+        await interaction.followup.send("セキュリティ設定を更新しました。", ephemeral=True)
+
+    @app_commands.command(name="ngword-add", description="NGワードを追加します")
+    @is_admin()
+    async def ngword_add(self, interaction: discord.Interaction, word: str):
+        await interaction.response.defer(ephemeral=True)
+        guild_id = interaction.guild_id
+        ng_words = storage.get_setting("security", guild_id, "ng_words", [])
+        if word not in ng_words:
+            ng_words.append(word)
+            storage.set_data("security", guild_id, "ng_words", ng_words)
+            await interaction.followup.send(f"NGワードに `{word}` を追加しました。", ephemeral=True)
+        else:
+            await interaction.followup.send("そのワードは既に追加されています。", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(SecurityCog(bot))
