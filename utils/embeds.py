e1e455a0a27d5afc5822b060a23a7f2a441f8c7c
LynelMC <narumarodayo@gmail.com>
2026-07-31 18:55:16 +0900

    Update embeds.py

--- utils/embeds.py
+++ utils/embeds.py
@@ -5,8 +5,8 @@ class EmbedUtils:
     @staticmethod
     def create_embed(title=None, description=None, color=discord.Color.blue(), thumbnail=None, image=None, footer_text=None, footer_icon=None, timestamp=False):
         embed = discord.Embed(
-            title=title if title else discord.Embed.Empty,
-            description=description if description else discord.Embed.Empty,
+            title=title if title else None,
+            description=description if description else None,
             color=color
         )
         if thumbnail:
@@ -14,7 +14,7 @@ class EmbedUtils:
         if image:
             embed.set_image(url=image)
         if footer_text:
-            embed.set_footer(text=footer_text, icon_url=footer_icon if footer_icon else discord.Embed.Empty)
+            embed.set_footer(text=footer_text, icon_url=footer_icon if footer_icon else None)
         if timestamp:
             embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
         return embed
